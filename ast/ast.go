002688104a6bfaabfa7cde9d54e9edb352131d04
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-05-29 20:03:55 -0700

    parse let statements

---
+++ ast/ast.go
@@ -0,0 +1,54 @@
+package ast
+
+import "github.com/axiom-labs/monkey/token"
+
+// Node is implemented for every node in our AST.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement interface for statements.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression interface for expressions.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of every AST our parser produces.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
